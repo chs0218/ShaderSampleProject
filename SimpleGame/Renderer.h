a58370ea6fa2d3906d54c452cde14b050c31c400
chs0218 <39898936+chs0218@users.noreply.github.com>
2023-04-07 15:49:40 +0900

    Import New Project

---
+++ SimpleGame/Renderer.h
@@ -0,0 +1,52 @@
+#pragma once
+
+#include <string>
+#include <cstdlib>
+#include <fstream>
+#include <iostream>
+
+#include "Dependencies\glew.h"
+
+class Renderer
+{
+public:
+	Renderer(int windowSizeX, int windowSizeY);
+	~Renderer();
+
+	bool IsInitialized();
+	void DrawSolidRect(float x, float y, float z, float size, float r, float g, float b, float a);
+	void Class0310_Rendering();
+	void DrawParticle();
+
+private:
+	void Initialize(int windowSizeX, int windowSizeY);
+	bool ReadFile(char* filename, std::string *target);
+	void AddShader(GLuint ShaderProgram, const char* pShaderText, GLenum ShaderType);
+	GLuint CompileShaders(char* filenameVS, char* filenameFS);
+	void CreateVertexBufferObjects();
+	void GetGLPosition(float x, float y, float *newX, float *newY);
+	void Class0310();
+	void CreateParticleVBO(int numParticleCount);
+	GLuint m_ParticleShader = -1;
+	GLuint m_ParticleVBO = -1;
+	GLuint m_ParticleVelVBO = -1;
+	GLuint m_ParticleEmitTimeVBO = -1;
+	GLuint m_ParticleLifeTimeVBO = -1;
+	GLuint m_ParticlePeriodVBO = -1;
+	GLuint m_ParticleAmpVBO = -1;
+	GLuint m_ParticleValueVBO = -1;
+	GLuint m_ParticleVertexCount = -1;
+
+	bool m_Initialized = false;
+	
+	unsigned int m_WindowSizeX = 0;
+	unsigned int m_WindowSizeY = 0;
+
+	GLuint m_VBORect = 0;
+	GLuint m_SolidRectShader = 0;
+
+	GLuint m_testVBO = 0;
+	GLuint m_testVBO1 = 0;
+	GLuint m_ColorVBO = 0;
+};
+
--- SimpleGame/SimpleGame/Shaders/Particle.fs
+++
@@ -1,9 +0,0 @@
-#version 330
-
-layout(location=0) out vec4 FragColor;
-in vec4 v_Color;
-
-void main()
-{
-	FragColor = vec4(1.0f, 1.0f, 1.0f, 1.0f);
-}
